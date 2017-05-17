48f8e3972c54b5a7d7941d33305f8bf38e7fdcb1
Alex Burner <alex.burner@gmail.com>
2017-05-16 22:39:55 -0700

    unifying visualizer canvas

--- src/components/Visualizer.tsx
+++ src/components/Visualizer.tsx
@@ -5,64 +5,6 @@ import {togglePlay} from 'src/singletons/playlist';
 
 import Fullscreen from 'src/components/Fullscreen';
 
-const drawSpectro = (
-    canvas:HTMLCanvasElement,
-    context:CanvasRenderingContext2D,
-    channels:Uint8Array[]
-) => {
-    const width = canvas.width;
-    const height = canvas.height;
-    context.fillStyle = '#333';
-    context.fillRect(0, 0, width, height);
-    const rectCount = channels[0].length;
-    const rectWidth = width / 2 / rectCount;
-    channels.forEach((channel, index) => {
-        for (let i = 0; i < rectCount; i++) {
-            const magnitude = channel[i] / 255;
-            const hue = magnitude * 300;
-            const rectHeight = magnitude * height;
-            context.fillStyle = `hsl(${hue}, 64%, 64%)`;
-            context.fillRect(
-                index === 0 // left channel?
-                    ? width / 2 - rectWidth * i
-                    : width / 2 + rectWidth * i,
-                height - rectHeight,
-                rectWidth + 1,
-                rectHeight
-            );
-        }
-    });
-};
-
-const drawWaterfall = (
-    canvas:HTMLCanvasElement,
-    context:CanvasRenderingContext2D,
-    channels:Uint8Array[]
-) => {
-    const width = canvas.width;
-    const height = canvas.height;
-    const image = context.getImageData(0, 0, width, height);
-    const count = channels[0].length;
-    const rectWidth = width / 2 / count;
-    const rectHeight = rectWidth;
-    channels.forEach((channel, index) => {
-        for (let i = 0; i < count; i++) {
-            const magnitude = channel[i] / 255;
-            const hue = magnitude * 300;
-            context.fillStyle = `hsl(${hue}, 64%, 64%)`;
-            context.fillRect(
-                index === 0 // left channel?
-                    ? width / 2 - rectWidth * i
-                    : width / 2 + rectWidth * i,
-                0,
-                rectWidth + 1,
-                rectHeight + 1
-            );
-        }
-    });
-    context.putImageData(image, 0, rectHeight);
-};
-
 interface Props {
     width:number,
     height:number,
@@ -70,14 +12,14 @@ interface Props {
 }
 
 export default class Visualizer extends React.Component<Props, undefined> {
-    private spectroCanvas:HTMLCanvasElement;
-    private waterfallCanvas:HTMLCanvasElement;
-    private spectroContext:CanvasRenderingContext2D;
-    private waterfallContext:CanvasRenderingContext2D;
+    private canvas:HTMLCanvasElement;
+    private canvasCtx:CanvasRenderingContext2D;
     private mountSignature:{}; // object reference as id
     private width:number;
     private height:number;
     private border:number;
+    private spectHeight:number;
+    private waterHeight:number;
     private el:HTMLElement;
 
     constructor(props) {
@@ -85,14 +27,30 @@ export default class Visualizer extends React.Component<Props, undefined> {
         this.width = props.width;
         this.height = props.height;
         this.border = props.border;
+        this.spectHeight = 1 * (this.height / 6);
+        this.waterHeight = 5 * (this.height / 6);
     }
 
     animate(mountSignature:{}) {
         // bail if closure & class signature don't match
         if (mountSignature !== this.mountSignature) return;
         const channels = getChannels();
-        drawSpectro(this.spectroCanvas, this.spectroContext, channels);
-        drawWaterfall(this.waterfallCanvas, this.waterfallContext, channels);
+        drawSpectro({
+            x: 0,
+            y: 0,
+            width: this.width,
+            height: this.spectHeight,
+            context: this.canvasCtx,
+            channels,
+        });
+        drawWaterfall({
+            x: 0,
+            y: this.spectHeight,
+            width: this.width,
+            height: this.waterHeight,
+            context: this.canvasCtx,
+            channels,
+        });
         window.requestAnimationFrame(() => {
             this.animate(mountSignature);
         });
@@ -111,15 +69,9 @@ export default class Visualizer extends React.Component<Props, undefined> {
                 }}
             >
                 <canvas
-                    ref={el => this.spectroCanvas = el}
+                    ref={el => this.canvas = el}
                     style={{display: 'block'}}
-                    height={1 * (this.height / 6)}
-                    width={this.width}
-                />
-                <canvas
-                    ref={el => this.waterfallCanvas = el}
-                    style={{display: 'block'}}
-                    height={5 * (this.height / 6)}
+                    height={this.height}
                     width={this.width}
                 />
                 {this.el && <Fullscreen target={this.el} />}
@@ -130,8 +82,7 @@ export default class Visualizer extends React.Component<Props, undefined> {
     componentDidMount() {
         this.forceUpdate(); // render() with ref for Fullscreen
         this.mountSignature = {}; // unique object reference
-        this.spectroContext = this.spectroCanvas.getContext('2d');
-        this.waterfallContext = this.waterfallCanvas.getContext('2d');
+        this.canvasCtx = this.canvas.getContext('2d');
         this.animate(this.mountSignature);
     }
 
@@ -139,3 +90,58 @@ export default class Visualizer extends React.Component<Props, undefined> {
         this.mountSignature = null; // stops animation loop
     }
 }
+
+interface DrawArgs {
+    x:number;
+    y:number;
+    width:number;
+    height:number;
+    context:CanvasRenderingContext2D;
+    channels:Uint8Array[];
+}
+
+const drawSpectro = ({x, y, width, height, context, channels}:DrawArgs) => {
+    context.fillStyle = '#333';
+    context.fillRect(x, y, width, height);
+    const rectCount = channels[0].length;
+    const rectWidth = width / 2 / rectCount;
+    channels.forEach((channel, index) => {
+        for (let i = 0; i < rectCount; i++) {
+            const magnitude = channel[i] / 255;
+            const hue = magnitude * 300;
+            const rectHeight = magnitude * height;
+            context.fillStyle = `hsl(${hue}, 64%, 64%)`;
+            context.fillRect(
+                index === 0 // left channel?
+                    ? x + width / 2 - rectWidth * i
+                    : x + width / 2 + rectWidth * i,
+                y + height - rectHeight,
+                rectWidth + 1,
+                rectHeight
+            );
+        }
+    });
+};
+
+const drawWaterfall = ({x, y, width, height, context, channels}:DrawArgs) => {
+    const image = context.getImageData(x, y, width, height);
+    const count = channels[0].length;
+    const rectWidth = width / 2 / count;
+    const rectHeight = rectWidth;
+    channels.forEach((channel, index) => {
+        for (let i = 0; i < count; i++) {
+            const magnitude = channel[i] / 255;
+            const hue = magnitude * 300;
+            context.fillStyle = `hsl(${hue}, 64%, 64%)`;
+            context.fillRect(
+                index === 0
+                    ? x + width / 2 - rectWidth * i
+                    : x + width / 2 + rectWidth * i,
+                y,
+                rectWidth + 1,
+                rectHeight + 1
+            );
+        }
+    });
+    context.putImageData(image, x, y + rectHeight);
+};
